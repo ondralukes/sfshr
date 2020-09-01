3eb620329b2b64b9fa7bb68a73f4e6f39147144d
ondralukes <mail@ondralukes.cz>
2020-09-01 16:13:55 +0200

    Speed indicator

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,10 +7,11 @@ use crate::transfer::transfer::{Download, Upload};
 use std::convert::TryInto;
 use std::env::args;
 use std::fs::File;
-use std::io::{stdout, Read, Write};
+use std::io::{Read, Write, Seek, SeekFrom};
 use std::net::ToSocketAddrs;
 use std::path::PathBuf;
 use std::process::exit;
+use std::time::Instant;
 
 const BUFFER_SIZE: usize = 1024 * 1024;
 
@@ -61,6 +62,51 @@ fn main() {
     }
 }
 
+trait FormatSize {
+    fn format_size(self) -> String;
+}
+
+impl FormatSize for u64 {
+    fn format_size(self) -> String {
+        const PREFIXES: [&str; 5] = ["", "Ki", "Mi", "Gi", "Ti"];
+
+        let mut i: f64 = 1.0;
+        let mut index = 0;
+        loop {
+            if (self as f64 / i) < 500.0 {
+                break;
+            }
+
+            i *= 1024.0;
+            index += 1;
+        }
+
+        format!("{:.3} {}B", self as f64 / i, PREFIXES[index])
+    }
+}
+
+impl FormatSize for f64 {
+    fn format_size(self) -> String {
+        const PREFIXES: [&str; 5] = ["", "Ki", "Mi", "Gi", "Ti"];
+
+        let mut i: f64 = 1.0;
+        let mut index = 0;
+        if !self.is_normal(){
+            return String::from("NaN");
+        }
+        loop {
+            if (self / i) < 500.0 {
+                break;
+            }
+
+            i *= 1024.0;
+            index += 1;
+        }
+
+        format!("{:.3} {}B", self as f64 / i, PREFIXES[index])
+    }
+}
+
 fn upload<A: ToSocketAddrs>(addr: A, filepath: PathBuf, encrypt: bool) {
     let file = File::open(&filepath);
     if file.is_err() {
@@ -73,6 +119,8 @@ fn upload<A: ToSocketAddrs>(addr: A, filepath: PathBuf, encrypt: bool) {
     let mut upload = Upload::new(addr, encrypt).unwrap();
     let mut buf = Vec::new();
     buf.resize(BUFFER_SIZE, 0);
+
+    let time = Instant::now();
     loop {
         let bytes_read = file.read(&mut buf).unwrap();
         if bytes_read == 0 {
@@ -80,9 +128,21 @@ fn upload<A: ToSocketAddrs>(addr: A, filepath: PathBuf, encrypt: bool) {
         }
 
         upload.send(&buf[..bytes_read]).unwrap();
+        let time = time.elapsed().as_micros() as f64;
+        let size = file.seek(SeekFrom::Current(0)).unwrap();
+        let speed = size as f64/time*1000000.0;
+
+        println!("Uploaded {:^12} @ {:^12}  \x1b[1A\x1b[0G", size.format_size(), format!("{}/s", speed.format_size()));
     }
 
     upload.finalize().unwrap();
+    let time = time.elapsed().as_micros() as f64;
+    let size = file.seek(SeekFrom::Current(0)).unwrap();
+    let speed = size as f64/time*1000000.0;
+
+    println!("Uploaded {:^12} @ {:^12}  \x1b[1A\x1b[0G", size.format_size(), format!("{}/s",  speed.format_size()));
+
+
 
     let mut download_key = Vec::new();
     download_key.extend_from_slice(&upload.id());
@@ -109,6 +169,7 @@ fn download<A: ToSocketAddrs>(addr: A, download_key: Vec<u8>, encrypt: bool) {
     }
     let mut download = Download::new(addr, &download_key[..32].try_into().unwrap(), key).unwrap();
     let mut file = File::create("output").unwrap();
+    let time = Instant::now();
     loop {
         let (buf, cont) = download.read().unwrap();
         if !cont {
@@ -116,5 +177,12 @@ fn download<A: ToSocketAddrs>(addr: A, download_key: Vec<u8>, encrypt: bool) {
         }
 
         file.write_all(&buf).unwrap();
+        let time = time.elapsed().as_micros() as f64;
+        let size = file.seek(SeekFrom::Current(0)).unwrap();
+        let speed = size as f64/time*1000000.0;
+
+        println!("Downloaded {:^12} @ {:^12}\x1b[1A\x1b[0G", size.format_size(), format!("{}/s", speed.format_size()));
     }
+
+    println!();
 }
