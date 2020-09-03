aeb7303727971ca59d6d570c3c9dd95fc2bbf572
ondralukes <mail@ondralukes.cz>
2020-09-03 15:41:07 +0200

    Transfers tests

--- server/src/config.rs
+++ server/src/config.rs
@@ -1,64 +1,73 @@
 pub mod config {
-    use std::path::Path;
     use std::fs::File;
     use std::io::Read;
-    use std::str::FromStr;
+    use std::path::Path;
     use std::process::exit;
+    use std::str::FromStr;
 
-    pub struct Config{
+    pub struct Config {
         expiration: u64,
         thread_count: u64,
-        uploads: String
+        uploads: String,
     }
 
     impl Config {
-        pub fn new<P: AsRef<Path>>(config_file: P) -> Self{
+        pub fn new<P: AsRef<Path>>(config_file: P) -> Self {
             let file = File::open(config_file);
             if file.is_err() {
                 println!("Failed to open config file.");
             }
 
-            let mut expiration= 10800;
+            let mut expiration = 10800;
             let mut thread_count = 8;
             let mut uploads = String::from("uploads");
             let mut file = file.unwrap();
             let mut str = String::new();
             file.read_to_string(&mut str).unwrap();
             let mut line = 0;
-            for pair in str.split('\n'){
+            for pair in str.split('\n') {
                 line += 1;
                 let mut pair = pair.to_owned();
-                pair.retain(|x|{!x.is_whitespace()});
+                pair.retain(|x| !x.is_whitespace());
                 if pair.starts_with('#') || pair.len() == 0 {
                     continue;
                 }
-                let mut split = pair.splitn(2,'=').into_iter();
+                let mut split = pair.splitn(2, '=').into_iter();
 
                 let key = split.next().unwrap();
                 let value = split.next();
                 if value.is_none() {
-                    println!("Config parsing failed: not a key-pair value at line {}", line);
+                    println!(
+                        "Config parsing failed: not a key-pair value at line {}",
+                        line
+                    );
                     exit(1);
                 }
                 let value = value.unwrap();
 
-                if key == "EXPIRATION_TIME"{
+                if key == "EXPIRATION_TIME" {
                     let parse = u64::from_str(value);
                     if parse.is_err() {
-                        println!("Config parsing failed: failed to parse \"{}\" as u64 at line {}", value, line);
+                        println!(
+                            "Config parsing failed: failed to parse \"{}\" as u64 at line {}",
+                            value, line
+                        );
                         exit(1);
                     }
 
                     expiration = parse.unwrap();
-                } else if key == "THREAD_COUNT"{
+                } else if key == "THREAD_COUNT" {
                     let parse = u64::from_str(value);
                     if parse.is_err() {
-                        println!("Config parsing failed: failed to parse \"{}\" as u64 at line {}", value, line);
+                        println!(
+                            "Config parsing failed: failed to parse \"{}\" as u64 at line {}",
+                            value, line
+                        );
                         exit(1);
                     }
 
                     thread_count = parse.unwrap();
-                } else if key == "UPLOADS"{
+                } else if key == "UPLOADS" {
                     uploads = String::from(value);
                 } else {
                     println!("Warning! Found unknown key {} in config file.", key);
@@ -68,7 +77,7 @@ pub mod config {
             Self {
                 expiration,
                 thread_count,
-                uploads
+                uploads,
             }
         }
 
@@ -78,17 +87,17 @@ pub mod config {
         pub fn thread_count(&self) -> u64 {
             self.thread_count
         }
-        pub fn uploads(&self) -> &String{
+        pub fn uploads(&self) -> &String {
             &self.uploads
         }
     }
 
-    impl Clone for Config{
+    impl Clone for Config {
         fn clone(&self) -> Self {
             Self {
                 expiration: self.expiration,
                 thread_count: self.thread_count,
-                uploads: self.uploads.clone()
+                uploads: self.uploads.clone(),
             }
         }
     }
---
+++ tests/tests/transfers.rs
@@ -0,0 +1,178 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::fs;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::ops::Deref;
+use std::path::Path;
+use std::process::{Child, Command, Stdio};
+use std::sync::Mutex;
+
+static mut SERVER: Option<Child> = None;
+lazy_static! {
+    static ref MUTEX: Mutex<()> = Mutex::new(());
+}
+
+#[test]
+fn encrypted_transfer() {
+    let _guard = MUTEX.deref().lock().unwrap();
+    unsafe {
+        SERVER = Some(
+            Command::new("cargo")
+                .stderr(Stdio::null())
+                .stdout(Stdio::null())
+                .args(&["run", "--", "--config", "../tests/tests/normal-config"])
+                .current_dir("../server")
+                .spawn()
+                .unwrap_or_else(unwrap_clean_up),
+        );
+    }
+    generate_test_file();
+    let sender = Command::new("cargo")
+        .stderr(Stdio::null())
+        .stdout(Stdio::piped())
+        .args(&["run", "--", "--quiet", "test-file"])
+        .current_dir("../client")
+        .spawn()
+        .unwrap_or_else(unwrap_clean_up);
+    let sender_output = sender.wait_with_output().unwrap_or_else(unwrap_clean_up);
+    if !sender_output.status.success() {
+        clean_up();
+        println!(
+            "---stdout---\n {}",
+            String::from_utf8(sender_output.stdout).unwrap()
+        );
+        panic!("Sender exited with non-zero exit code.");
+    }
+    remove_test_file();
+    let mut link = String::from_utf8(sender_output.stdout).unwrap_or_else(unwrap_clean_up);
+    link = link.replace('\n', "");
+    let link_args: Vec<&str> = link.split(' ').collect();
+
+    println!("{:?}", link_args);
+    let receiver = Command::new("cargo")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::null())
+        .args(&["run", "--", "--quiet"])
+        .args(&link_args[1..])
+        .current_dir("../client")
+        .spawn()
+        .unwrap();
+
+    let receiver_output = receiver.wait_with_output().unwrap_or_else(unwrap_clean_up);
+    if !receiver_output.status.success() {
+        clean_up();
+        println!(
+            "---stdout---\n {}",
+            String::from_utf8(receiver_output.stdout).unwrap()
+        );
+        panic!("Receiver exited with non-zero exit code.");
+    }
+    check_test_file("../client/test-file");
+    clean_up();
+}
+
+#[test]
+fn unencrypted_transfer() {
+    let _guard = MUTEX.deref().lock().unwrap();
+    unsafe {
+        SERVER = Some(
+            Command::new("cargo")
+                .stderr(Stdio::null())
+                .stdout(Stdio::null())
+                .args(&["run", "--", "--config", "../tests/tests/normal-config"])
+                .current_dir("../server")
+                .spawn()
+                .unwrap_or_else(unwrap_clean_up),
+        );
+    }
+    generate_test_file();
+    let sender = Command::new("cargo")
+        .stderr(Stdio::null())
+        .stdout(Stdio::piped())
+        .args(&["run", "--", "--quiet", "--no-encryption", "test-file"])
+        .current_dir("../client")
+        .spawn()
+        .unwrap_or_else(unwrap_clean_up);
+    let sender_output = sender.wait_with_output().unwrap_or_else(unwrap_clean_up);
+    if !sender_output.status.success() {
+        clean_up();
+        println!(
+            "---stdout---\n {}",
+            String::from_utf8(sender_output.stdout).unwrap()
+        );
+        panic!("Sender exited with non-zero exit code.");
+    }
+    remove_test_file();
+    let mut link = String::from_utf8(sender_output.stdout).unwrap_or_else(unwrap_clean_up);
+    link = link.replace('\n', "");
+    let link_args: Vec<&str> = link.split(' ').collect();
+
+    println!("{:?}", link_args);
+    let receiver = Command::new("cargo")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::null())
+        .args(&["run", "--", "--quiet"])
+        .args(&link_args[1..])
+        .current_dir("../client")
+        .spawn()
+        .unwrap();
+
+    let receiver_output = receiver.wait_with_output().unwrap_or_else(unwrap_clean_up);
+    if !receiver_output.status.success() {
+        clean_up();
+        println!(
+            "---stdout---\n {}",
+            String::from_utf8(receiver_output.stdout).unwrap()
+        );
+        panic!("Receiver exited with non-zero exit code.");
+    }
+    check_test_file("../client/test-file");
+    clean_up();
+}
+
+fn remove_test_file() {
+    let client_temp = Path::new("../client/test-file");
+    if client_temp.exists() {
+        fs::remove_file(client_temp).unwrap();
+    }
+}
+
+fn generate_test_file() {
+    let mut file = File::create("../client/test-file").unwrap();
+    let mut buffer = Vec::new();
+    buffer.resize(1024 * 1024 * 64, 12);
+    file.write_all(&buffer).unwrap();
+}
+
+fn check_test_file<P: AsRef<Path>>(path: P) {
+    let mut file = File::open(path).unwrap();
+    let mut buf = Vec::new();
+    buf.resize(1024 * 1024, 0);
+    loop {
+        let bytes_read = file.read(&mut buf).unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+        assert_eq!(buf[..bytes_read].iter().find(|&&x| x != 12), None);
+    }
+}
+
+fn unwrap_clean_up<T, E>(_: E) -> T {
+    clean_up();
+    panic!();
+}
+
+fn clean_up() {
+    remove_test_file();
+    let uploads = Path::new("../server/test-uploads");
+    if uploads.exists() {
+        fs::remove_dir_all(uploads).unwrap();
+    }
+    unsafe {
+        if SERVER.is_some() {
+            SERVER.as_mut().unwrap().kill().unwrap();
+        }
+    }
+}
