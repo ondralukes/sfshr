f44e0f5a885cb7d709890d2b660a97fc67e817aa
ondralukes <mail@ondralukes.cz>
2020-09-08 19:51:38 +0200

    Client server select

--- tests/tests/transfers.rs
+++ tests/tests/transfers.rs
@@ -36,7 +36,7 @@ fn encrypted_transfer() {
     let sender = Command::new("cargo")
         .stderr(Stdio::inherit())
         .stdout(Stdio::piped())
-        .args(&["run", "--", "--quiet", "test-file"])
+        .args(&["run", "--", "--quiet", "--server", "localhost:40788", "test-file"])
         .current_dir("../client")
         .spawn()
         .unwrap_or_else(unwrap_clean_up);
@@ -95,7 +95,7 @@ fn unencrypted_transfer() {
     let sender = Command::new("cargo")
         .stderr(Stdio::inherit())
         .stdout(Stdio::piped())
-        .args(&["run", "--", "--quiet", "--no-encryption", "test-file"])
+        .args(&["run", "--", "--quiet", "--server", "localhost:40788", "--no-encryption", "test-file"])
         .current_dir("../client")
         .spawn()
         .unwrap_or_else(unwrap_clean_up);
@@ -154,7 +154,7 @@ fn expired() {
     let sender = Command::new("cargo")
         .stderr(Stdio::inherit())
         .stdout(Stdio::piped())
-        .args(&["run", "--", "--quiet", "--no-encryption", "test-file"])
+        .args(&["run", "--", "--quiet", "--server", "localhost:40788", "--no-encryption", "test-file"])
         .current_dir("../client")
         .spawn()
         .unwrap_or_else(unwrap_clean_up);
@@ -213,7 +213,7 @@ fn size_exceeded() {
     let sender = Command::new("cargo")
         .stderr(Stdio::inherit())
         .stdout(Stdio::piped())
-        .args(&["run", "--", "--quiet", "--no-encryption", "test-file"])
+        .args(&["run", "--", "--quiet", "--server", "localhost:40788", "--no-encryption", "test-file"])
         .current_dir("../client")
         .spawn()
         .unwrap_or_else(unwrap_clean_up);
