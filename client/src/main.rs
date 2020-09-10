ef69b0b7d397dd3d99ab25711d31e14ad3505fdc
ondralukes <mail@ondralukes.cz>
2020-09-10 16:59:17 +0200

    --tar option

--- client/src/main.rs
+++ client/src/main.rs
@@ -12,7 +12,7 @@ use std::fs::File;
 use std::net::ToSocketAddrs;
 use std::path::{Path, PathBuf};
 use std::process::exit;
-use std::{fmt, fs};
+use std::{fmt, fs, io};
 use tar::{Archive, Builder};
 
 fn main() {
@@ -21,6 +21,7 @@ fn main() {
 
     let mut encrypt = true;
     let mut receive = false;
+    let mut keep_tar = None;
     let mut quiet = false;
     let mut main_arg = None;
     let mut server = String::from("ondralukes.cz:40788");
@@ -38,6 +39,7 @@ fn main() {
                 } else if arg == "--help" {
                     println!("Usage: sfshr [file] or sfshr -r [download key]");
                     println!(" -r [download key] - download file");
+                    println!(" -t --tar [tarname] - store downloaded tar as [tarname], instead of unpacking it");
                     println!(" -n --no-encryption - do not encrypt or decrypt the file");
                     println!(" -q --quiet - do not print anything (expect download key)");
                     println!(" -s --server [hostname:port] - specify sfshr server (default: 'ondralukes.cz:40788')");
@@ -54,6 +56,16 @@ fn main() {
                             server = val;
                         }
                     }
+                } else if arg == "-t" || arg == "--tar" {
+                    match args.next() {
+                        None => {
+                            println!("Expected value for --tar");
+                            exit(1);
+                        }
+                        Some(val) => {
+                            keep_tar = Some(val);
+                        }
+                    }
                 } else {
                     main_arg = Some(arg);
                 }
@@ -68,7 +80,7 @@ fn main() {
         }
 
         let path = PathBuf::from(main_arg.unwrap());
-        upload(server, path, encrypt, quiet);
+        upload(server, path, encrypt, quiet, keep_tar);
     } else {
         if main_arg.is_none() {
             printinfoln!(quiet, "No download key specified!");
@@ -79,11 +91,17 @@ fn main() {
             printinfoln!(quiet, "Invalid download key format!");
             exit(1);
         }
-        download(server, download_key.unwrap(), encrypt, quiet);
+        download(server, download_key.unwrap(), encrypt, quiet, keep_tar);
     }
 }
 
-fn upload(addr: String, mut filepath: PathBuf, encrypt: bool, quiet: bool) {
+fn upload(
+    addr: String,
+    mut filepath: PathBuf,
+    encrypt: bool,
+    quiet: bool,
+    keep_tar: Option<String>,
+) {
     let mut upload = Upload::new(&addr, encrypt, quiet).unwrap_or_else(on_error);
     let mut archive = Builder::new(upload);
 
@@ -121,22 +139,32 @@ fn upload(addr: String, mut filepath: PathBuf, encrypt: bool, quiet: bool) {
         download_key.extend_from_slice(upload.key().unwrap());
     }
 
-    let mut server_extra = String::new();
+    let mut extras = String::new();
     if &addr != "ondralukes.cz:40788" {
-        server_extra = format!(" --server {}", addr);
+        extras = format!(" --server {}", addr);
+    }
+
+    if keep_tar.is_some() {
+        extras.push_str(&format!(" --tar {}", keep_tar.unwrap()));
     }
     if encrypt {
-        println!("sfshr{} -r {}", server_extra, base64::encode(&download_key));
+        println!("sfshr{} -r {}", extras, base64::encode(&download_key));
     } else {
         println!(
             "sfshr --no-encryption{} -r {}",
-            server_extra,
+            extras,
             base64::encode(&download_key)
         );
     }
 }
 
-fn download<A: ToSocketAddrs>(addr: A, download_key: Vec<u8>, encrypt: bool, quiet: bool) {
+fn download<A: ToSocketAddrs>(
+    addr: A,
+    download_key: Vec<u8>,
+    encrypt: bool,
+    quiet: bool,
+    keep_tar: Option<String>,
+) {
     if (encrypt && download_key.len() != 64) || (!encrypt && download_key.len() != 32) {
         printinfoln!(quiet, "Invalid download key size!");
         exit(1);
@@ -146,45 +174,58 @@ fn download<A: ToSocketAddrs>(addr: A, download_key: Vec<u8>, encrypt: bool, qui
     if encrypt {
         key = Some(download_key[32..].try_into().unwrap());
     }
-    let download =
+    let mut download =
         Download::new(addr, &download_key[..32].try_into().unwrap(), key).unwrap_or_else(on_error);
 
-    let mut archive = Archive::new(download);
-    let mut iter = archive.entries().unwrap();
-    let mut first = iter.next().unwrap().unwrap();
+    match keep_tar {
+        None => {
+            let mut archive = Archive::new(download);
+            let mut iter = archive.entries().unwrap();
+            let mut first = iter.next().unwrap().unwrap();
 
-    let archive_root;
+            let archive_root;
 
-    let first_path = Path::new(".").join(first.path().unwrap());
-    let mut fp_iter = first_path.iter();
+            let first_path = Path::new(".").join(first.path().unwrap());
+            let mut fp_iter = first_path.iter();
 
-    //Skip '.'
-    fp_iter.next();
-    let first_path = Path::new(fp_iter.next().unwrap());
-    if first_path.exists() {
-        printinfoln!(
-            quiet,
-            "Cannot write to {:?}. Destination already exists.",
-            first_path
-        );
-        exit(1);
-    } else {
-        archive_root = first_path.to_str().unwrap().to_string();
+            //Skip '.'
+            fp_iter.next();
+            let first_path = Path::new(fp_iter.next().unwrap());
+            if first_path.exists() {
+                printinfoln!(
+                    quiet,
+                    "Cannot write to {:?}. Destination already exists.",
+                    first_path
+                );
+                exit(1);
+            } else {
+                archive_root = first_path.to_str().unwrap().to_string();
+            }
+
+            first.unpack_in(".").unwrap();
+
+            for entry in iter {
+                let mut entry = entry.unwrap();
+                entry.unpack_in(".").unwrap();
+            }
+
+            printinfoln!(quiet, "");
+            printinfoln!(
+                quiet,
+                "\x1b[1A\x1b[0G\x1b[KSuccesfully downloaded {:?}",
+                archive_root
+            );
+        }
+        Some(dest) => {
+            let mut file = File::create(&dest).unwrap_or_else(on_error);
+            io::copy(&mut download, &mut file).unwrap_or_else(on_error);
+            printinfoln!(
+                quiet,
+                "\x1b[1A\x1b[0G\x1b[KSuccesfully downloaded {:?}",
+                dest
+            );
+        }
     }
-
-    first.unpack_in(".").unwrap();
-
-    for entry in iter {
-        let mut entry = entry.unwrap();
-        entry.unpack_in(".").unwrap();
-    }
-
-    printinfoln!(quiet, "");
-    printinfoln!(
-        quiet,
-        "\x1b[1A\x1b[0G\x1b[KSuccesfully downloaded {:?}",
-        archive_root
-    );
 }
 
 fn on_error<E: fmt::Display, T>(err: E) -> T {
